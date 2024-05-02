f8b8d66986aa2ddce8bc224633b9e479abb01f9a
a23dilel <a23dilel@student.his.se>
2024-05-02 19:01:23 +0200

    Created a web view for load the HTML-file and enable javaScript in MainActivity.java

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        WebView webView = findViewById(R.id.my_webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
 
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
