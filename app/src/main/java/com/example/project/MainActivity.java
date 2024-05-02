aa796ee891293a9c1466f41d4b950ef782b6649a
a23dilel <a23dilel@student.his.se>
2024-05-02 17:34:24 +0200

    Created onCreated method in MainActivity.java

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.project;
 
-public class MainActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
